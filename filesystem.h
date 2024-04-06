49a2dec21fed822b7e88d7de1d1e584797d85a4d
sean-obeirne <sean.t.obeirne@gmail.com>
2024-04-06 16:06:15 -0400

    created more function declarations

--- filesystem.h
+++ filesystem.h
@@ -12,13 +12,15 @@
 #define DISK_SIZE 1024 // disk is this many blocks
 #define BLOCK_SIZE 4096 // TODO SEAN: make this pull from somewhere
 #define SECTOR_SIZE 512 // 8 sectors = 1 block
+#define ROOT_DIRECTORY_ENTRIES 32
+// #define ROOT_DIRECTORY_ENTRIES 4
 #define MAX_FILENAME_LENGTH 255
+#define FS_BUFFER_SIZE 4096
 // #define ROOT_LEN 512 // how many files can be in root?
 
-extern char *fs_buffer;
 
 typedef struct {
-    char filename[MAX_FILENAME_LENGTH];  // Name of the file or directory
+    char filename[MAX_FILENAME_LENGTH + 1];  // Name of the file or directory
     uint32_t size;                       // Size of the file in bytes
     uint8_t attributes;                  // Attributes of the file (e.g., read-only, hidden, directory)
     uint32_t block;                      // Starting block of the file's data
@@ -75,46 +77,89 @@ typedef struct {
     int last_error;
 } FileSystem;
 
-// Function prototypes for FAT32 filesystem operations
+/*
+** Dump root directory to CIO
+** @return 0 on success, -1 on failure.
+*/
+int dump_root( void );
 
-/**
- * Initialize the FAT32 filesystem.
- * @param fs Pointer to the filesystem structure to initialize.
- * @param disk Pointer to the disk device.
- * @return 0 on success, -1 on failure.
- */
-int _fs_init( void );
 
-void clear_fs_buffer();
+/*
+** Init the filesystem.
+** @return 0 on success, -1 on failure.
+*/
+int _fs_init(void);
 
-/**
- * Mount the FAT32 filesystem.
- * @param fs Pointer to the filesystem structure to mount.
- * @return 0 on success, -1 on failure.
- */
-int _fs_mount( void );
+/*
+** Mount the filesystem.
+** @return 0 on success, -1 on failure.
+*/
+int _fs_mount(void);
 
-/**
- * Read data from a file in the FAT32 filesystem.
- * @param fs Pointer to the mounted filesystem structure.
- * @param filename Name of the file to read.
- * @param buffer Pointer to the buffer to store the read data.
- * @param size Number of bytes to read.
- * @param offset Offset within the file to start reading from.
- * @return Number of bytes read on success, -1 on failure.
- */
-int _fs_read_file(const char *filename);
+/*
+** Read data from a file in the filesystem.
+** @param filename Name of the file to read.
+** @param buffer Pointer to the buffer to store the read data.
+** @param size Number of bytes to read.
+** @param offset Offset within the file to start reading from.
+** @return Number of bytes read on success, -1 on failure.
+*/
+int _fs_read_file(const char *filename/*, void *buffer, size_t size, off_t offset*/);
 
-/**
- * Write data to a file in the FAT32 filesystem.
- * @param fs Pointer to the mounted filesystem structure.
- * @param filename Name of the file to write.
- * @param buffer Pointer to the data to write.
- * @param size Number of bytes to write.
- * @param offset Offset within the file to start writing to.
- * @return Number of bytes written on success, -1 on failure.
- */
-int _fs_write_file(const char *filename);
+/*
+** Write data to a file in the filesystem.
+** @param filename Name of the file to write to.
+** @param buffer Pointer to the buffer containing the data to write.
+** @param size Number of bytes to write.
+** @param offset Offset within the file to start writing from.
+** @return Number of bytes written on success, -1 on failure.
+*/
+int _fs_write_file(const char *filename/*, const void *buffer, size_t size, off_t offset*/);
+
+/*
+** Create a new file in the filesystem.
+** @param filename Name of the file to create.
+** @return 0 on success, -1 on failure.
+*/
+int _fs_create_file(const char *filename);
+
+/*
+** Delete a file from the filesystem.
+** @param filename Name of the file to delete.
+** @return 0 on success, -1 on failure.
+*/
+int _fs_delete_file(const char *filename);
+
+/*
+** Rename a file in the filesystem.
+** @param old_filename Current name of the file.
+** @param new_filename New name for the file.
+** @return 0 on success, -1 on failure.
+*/
+int _fs_rename_file(const char *old_filename, const char *new_filename);
+
+/*
+** Open a file in the filesystem for reading or writing.
+** @param filename Name of the file to open.
+** @param mode Mode in which to open the file ("r" for read, "w" for write).
+** @return File descriptor on success, -1 on failure.
+*/
+int _fs_open_file(const char *filename, const char *mode);
+
+/*
+** Close an open file in the filesystem.
+** @param fd File descriptor of the file to close.
+** @return 0 on success, -1 on failure.
+*/
+int _fs_close_file(const char *filename);
+
+/*
+** Set the permissions of a file in the filesystem.
+** @param filename Name of the file to set permissions for.
+** @param permissions New permissions for the file.
+** @return 0 on success, -1 on failure.
+*/
+// int _fs_set_permissions(const char *filename, mode_t permissions);
 
 // Add more function prototypes as needed for your FAT32 filesystem implementation
 
